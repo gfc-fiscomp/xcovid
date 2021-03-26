bbc9589620f64cca4e7dbd504addc140eab5d9de
mgteus <mateus.guimaraes048@gmail.com>
2021-03-26 08:19:11 -0300

    Atualização.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import time
 import base64
 import pandas as pd
 import numpy as np 
+#from call_model import predict
 st.set_page_config(page_title="GFC-FiscomP", page_icon="💻️", layout="centered")
 #PAGE_CONFIG = {"apge_title":"TesteGFC", "page_icon":"smiley", "layuot":"centered"}
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -28,8 +29,9 @@ def main():
 		st.text("Oferecimento:")
 		col1, col2 = st.beta_columns(2)
 		#col1.markdown("![LogoGIF](https://media.giphy.com/media/4MXP8s6bSQC9LmMwme/giphy.gif)")
-		col1.image("https://media.giphy.com/media/4MXP8s6bSQC9LmMwme/giphy.gif", width=300, caption="Ref:https://media.giphy.com/media/4MXP8s6bSQC9LmMwme/giphy.gif" )
-		col2.markdown("**IMAGEM POATEK SCITEK DATATHON SEI LA**")
+		col1.image("https://media.giphy.com/media/4MXP8s6bSQC9LmMwme/giphy.gif", width=300, caption="Fonte:https://media.giphy.com/media/4MXP8s6bSQC9LmMwme/giphy.gif" )
+		col2.image("https://raw.githubusercontent.com/gfc-fiscomp/xcovid/main/scitekPoatek.png", caption="Fonte:https://poatek.com/scitek/", width=300)
+		#col2.markdown("![LogoScitek](https://github.com/gfc-fiscomp/xcovid/blob/main/scitekPoatek.png)")
 
 		
 		
@@ -39,10 +41,10 @@ def main():
 	elif choice == "Teste":
 		st.title("Classificação de imagens usando Redes Neurais Convolucionais")
 		st.header("Diagnóstico de COVID a partir de Raio-X pulmonar")
-		file = st.file_uploader("Envie aqui o raio-x:", type=["jpg", "png"])
+		fueto = st.file_uploader("Envie aqui o raio-x:", type=["jpg", "png"])
 		bot = st.button("Enviar")
-		if file is not None:
-			image = Image.open(file)
+		if fueto is not None:
+			image = Image.open(fueto)
 			st.image(image, caption="Arquivo enviado", use_column_width=True)
 			st.write("")
 			st.write("Classificando...")
@@ -51,8 +53,16 @@ def main():
 			for i in range(100):
 				latest_it.text(f'Etapa {i+1}/100')
 				bar.progress(i+1)
-				time.sleep(0.1)
-			st.success("Parabéns você enviou uma foto!")
+				time.sleep(0.05)
+			x = st.number_input("Digite aqui:", -1,1)
+			#x = predict(fueto)[0]
+			if x == -1:
+				st.markdown("**Calculando**")
+			elif x == 0:
+				st.success("COVID-19 Negativo")
+			else:
+				st.warning("COVID-19 Positivo")
+			
 			
 				
 			
