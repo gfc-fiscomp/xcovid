f56a4c29de4ef98fe5d227eba59a3c7a9baf4f8e
Marcos Tidball <marcostidball@gmail.com>
2021-03-26 00:26:57 -0300

    ADD script para inferencia

---
+++ call_model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torchvision import models
+from PIL import Image
+
+def load_model(model_path):
+    # instantiate the model
+    model = models.resnet50(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Sequential(nn.Linear(num_ftrs, 2))
+
+    model = model.to('cpu')
+
+    # load the trained model weights
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    print('model loaded successfully!')
+
+    return model
+
+def predict(image_path, save_path):
+    # load image
+    img = Image.open(image_path)
+
+    # define transforms
+    t = transforms.Compose([transforms.ToTensor(), transforms.Resize(224), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # apply transforms and turn image into batch
+    batch = torch.unsqueeze(t(img), 0)
+
+    # load model
+    model = load_model(save_path)
+
+    # predict
+    out = model(batch)
+
+    # define map 
+    m = {0: 'COVID-19 Negativo', 1: 'COVID-19 Positivo'}
+
+    prob = nn.functional.softmax(out, dim=1)[0] * 100
+    _, indices = torch.sort(out, descending=True)    
+
+    return [m[i], prob[i].item() for i in indices[0]]
+
+
